82ba38a7e69dcda787017d7930cb6823b159a1bd
Chetan Raval <chetanbraval@gmail.com>
2022-05-25 16:58:34 +1000

    added for loop to complete process_image_pliman()

--- disease_assessment.R
+++ disease_assessment.R
@@ -3,6 +3,9 @@
 # implement gravity in avg_bgcolor
 # combine duplicate values in phenotyping data 
 
+# load functions from Ido's gist
+devtools::source_gist("7f63547158ecdbacf31b54a58af0d1cc", filename = "util.R")
+
 # install/load packages in one-liner
 library(pacman)
 # load/install from GitHub repos
@@ -15,7 +18,7 @@ h <- image_import("./palette/h.png")
 s <- image_import("./palette/s.png")
 b <- image_import("./palette/b.JPEG")
 
-plan(multisession, workers = future::availableCores()-1 ) # automatically detect available cores
+plan(multisession, workers = future::availableCores()-2 ) # automatically detect available cores
 
 # read phebnotypic data and pivot (1 row per plant)
 discard_cols <- c("ID", "Start time", "Completion time", "Email", "Name", "Comments", "Images")
@@ -145,34 +148,109 @@ bioassay_files <- tibble(original_file = list.files("../../Image Capture/2022042
   separate(meta_string, into = c("bioassay", "pot", "plant", "replicate")) %>% 
   mutate(pot=as.integer(sub("POT", "", pot, ignore.case = TRUE)), plant=sub("PL", "", plant, ignore.case = TRUE))
 
-# subset a range of pots (figure our cropping region)
+# PROCESS THESE! #####
+# subset a range of pots (figure our cropping region);10
 bioassay_test <- bioassay_files %>% filter(pot<60) %>% .$original_file
 # for this set the crop parameters are: trim_bottom=230, trim_top=0,  trim_left=0, trim_right=400
+
+step_size <- 20
+
+steps <- seq(1,nrow(bioassay_files), by = step_size)
+combined_disease_assessment_results <- vector("list", length = length(steps))
+for(i in steps){
+  # i =421 
+  
+  next_step <- min(c(i+step_size-1, nrow(bioassay_files)))
+  LogMsg(glue::glue("Processing images {i} to {next_step}, please be patient..."))
+   # %>% process_image_pliman()
+  tic() # start timer
+  with_progress({
+    p <- progressor(steps = length(bioassay_files$original_file[i:next_step])) # 
+    combined_disease_assessment_results[[which(steps==i)]] <- bioassay_files$original_file[i:next_step] %>% 
+      future_map_dfr(.f = ~{
+        res_tibble <- process_image_pliman(image_file = .x, 
+                                           out_folder = "../../Image Capture/20220429_Phenotyping/processed_images", 
+                                           assess_disease = TRUE,
+                                           crop = TRUE, 
+                                           trim_bottom=350, trim_top=0,
+                                           trim_left=0, trim_right=230,
+                                           save_cropped = TRUE,
+                                           trans = TRUE,
+                                           set_fuzz = 18,
+                                           reference="#DAD1C8",
+                                           start_point = "+5400+3500",
+                                           h_pal = h, s_pal = s, b_pal = b)
+        p() # because we put this last in the future_map_dfr, the function returned the progressor step instead of the tibble!
+        return(res_tibble) # this should fix it...
+      }, .options = furrr_options(seed = TRUE)) # this removes the annoying warning about the seed (though I don't think we're generating any random numbers)
+  })
+  toc() 
+  
+}
+# combine results
+
+  # process disease assessment results ####
+combined_results_df <- map_dfr(combined_disease_assessment_results, bind_rows) %>% # check results and extract metadata from filename
+  mutate(meta_string=tools::file_path_sans_ext(basename(original_file))) %>% 
+  separate(meta_string, into = c("bioassay", "pot", "plant", "replicate")) %>% 
+  mutate(pot=sub("POT", "", pot, ignore.case = TRUE), plant=sub("PL", "", plant, ignore.case = TRUE)) %>% 
+  group_by(bioassay, pot, plant) %>% 
+  summarise(symptomatic_mean = mean(symptomatic, na.rm = TRUE), assessment_SD=sd(symptomatic, na.rm = TRUE), 
+            image_num=n()) %>% 
+  left_join(phenotyping_data ) %>% relocate(date, week, .before = 1) %>% 
+  write_csv("output/0220429_Phenotyping_processed_data.csv")
+
 
 tic() # start timer
 with_progress({
   p <- progressor(steps = length(bioassay_test)) # 
   disease_assessment_table <- bioassay_test %>% 
     future_map_dfr(.f = ~{
+      p()
       res_tibble <- process_image_pliman(image_file = .x, 
                                          out_folder = "../../Image Capture/20220429_Phenotyping/processed_images", 
                                          assess_disease = TRUE,
                                          crop = TRUE, 
-                                         trim_bottom=230, trim_top=0,
-                                         trim_left=0, trim_right=400,
+                                         trim_bottom=350, trim_top=0,
+                                         trim_left=0, trim_right=230,
                                          save_cropped = TRUE,
                                          trans = TRUE,
                                          set_fuzz = 18,
                                          reference="#DAD1C8",
                                          start_point = "+5400+3500",
                                          h_pal = h, s_pal = s, b_pal = b)
-      p() # because we put this last in the future_map_dfr, the function returned the progressor step instead of the tibble!
+      # because we put this last in the future_map_dfr, the function returned the progressor step instead of the tibble!
       return(res_tibble) # this should fix it...
-    }, .options = furrr_options(seed = TRUE)) # this removes the annoying warning about the seed (though I don'tthink we're generating any random numbers)
+    }, .options = furrr_options(seed = TRUE)) # this removes the annoying warning about the seed (though I don't think we're generating any random numbers)
 })
-toc()
+toc() # 248.672 sec elapsed
+# run the same for only cropped files
 
-
+# cropped_files <- list.files("../../Image Capture/20220429_Phenotyping/processed_images", "B001_POT13_.+_cropped.jpg", full.names = TRUE)
+# 
+# tic() # start timer
+# with_progress({
+#   p <- progressor(steps = length(cropped_files)) # 
+#   disease_assessment_cropped <- cropped_files %>% 
+#     future_map_dfr(.f = ~{
+#       p()
+#       process_image_pliman(image_file = .x, 
+#                                          out_folder = "../../Image Capture/20220429_Phenotyping/processed_images", 
+#                                          assess_disease = TRUE,
+#                                          crop = FALSE, 
+#                                          trim_bottom=230, trim_top=0,
+#                                          trim_left=0, trim_right=400,
+#                                          save_cropped = TRUE,
+#                                          trans = FALSE,
+#                                          set_fuzz = 18,
+#                                          reference="#DAD1C8",
+#                                          start_point = "+5400+3500",
+#                                          h_pal = h, s_pal = s, b_pal = b)
+#       # because we put this last in the future_map_dfr, the function returned the progressor step instead of the tibble!
+#       # return(cropped_res_tibble) # this should fix it...
+#     }, .options = furrr_options(seed = TRUE)) # this removes the annoying warning about the seed (though I don't think we're generating any random numbers)
+# })
+# toc()
 
 # process disease assessment results ####
 image_disease_data <- disease_assessment_table %>% # check results and extract metadata from filename
@@ -185,10 +263,63 @@ image_disease_data <- disease_assessment_table %>% # check results and extract m
   left_join(phenotyping_data ) %>% relocate(date, week, .before = 1) %>% 
   write_csv("output/B001_POT13-59_data.csv")
 
-# repeat for second subset ####
-# subset a range of pots (f60-100, igure our cropping region)
-bioassay_test <- bioassay_files %>% filter(between(pot, 60, 100)) %>% .$original_file
+
+# PROCESS POTS 60-100 ####
+# subset a range of pots (60-100, figure out cropping region)
+bioassay_test_2 <- bioassay_files %>% filter(between(pot, 60, 100)) %>% .$original_file
 # for this set the crop parameters are: trim_bottom=230, trim_top=0,  trim_left=0, trim_right=400
+# TEST FUZZ
+process_image_pliman(image_file = bioassay_test_2[2], 
+                     out_folder = "../../Image Capture/20220429_Phenotyping/processed_images", 
+                     assess_disease = FALSE,
+                     crop = TRUE, 
+                     trim_bottom=300, trim_top=100,
+                     trim_left=155, trim_right=270,
+                     save_cropped = TRUE,
+                     trans = TRUE,
+                     set_fuzz = 17,
+                     reference="#DAD1C8",
+                     start_point = "+5400+3500",
+                     h_pal = h, s_pal = s, b_pal = b)
+# bioassay_test <- list.files("../../Image Capture/20220429_Phenotyping", "B001_POT13_.+.JPG", full.names = TRUE)
+
+tic() # start timer
+with_progress({
+  p <- progressor(steps = length(bioassay_test_2)) # 
+  disease_assessment_table <- bioassay_test_2 %>% 
+    future_map_dfr(.f = ~{
+      res_tibble <- process_image_pliman(image_file = .x, 
+                                         out_folder = "../../Image Capture/20220429_Phenotyping/processed_images", 
+                                         assess_disease = TRUE,
+                                         crop = TRUE, 
+                                         trim_bottom=300, trim_top=100,
+                                         trim_left=155, trim_right=270,
+                                         save_cropped = TRUE,
+                                         trans = TRUE,
+                                         set_fuzz = 15,
+                                         reference="#DAD1C8",
+                                         start_point = "+5400+3500",
+                                         h_pal = h, s_pal = s, b_pal = b)
+      p() # because we put this last in the future_map_dfr, the function returned the progressor step instead of the tibble!
+      return(res_tibble) # this should fix it...
+    }, .options = furrr_options(seed = TRUE)) # this removes the annoying warning about the seed (though I don'tthink we're generating any random numbers)
+})
+toc()
+
+# process disease assessment results ####
+image_disease_data <- disease_assessment_table %>% # check results and extract metadata from filename
+  mutate(meta_string=tools::file_path_sans_ext(basename(original_file))) %>% 
+  separate(meta_string, into = c("bioassay", "pot", "plant", "replicate")) %>% 
+  mutate(pot=sub("POT", "", pot, ignore.case = TRUE), plant=sub("PL", "", plant, ignore.case = TRUE)) %>% 
+  group_by(bioassay, pot, plant) %>% 
+  summarise(symptomatic_mean = mean(symptomatic, na.rm = TRUE), assessment_SD=sd(symptomatic, na.rm = TRUE), 
+            image_num=n()) %>% 
+  left_join(phenotyping_data ) %>% relocate(date, week, .before = 1) %>% 
+  write_csv("output/B001_POT60-100_data.csv")
+
+# crop, transparent and assess whole folder
+
+bioassay_test <- list.files("../../Image Capture/20220429_Phenotyping", ".JPG", full.names = TRUE)
 
 tic() # start timer
 with_progress({
@@ -212,6 +343,7 @@ with_progress({
     }, .options = furrr_options(seed = TRUE)) # this removes the annoying warning about the seed (though I don'tthink we're generating any random numbers)
 })
 toc()
+
 
 
 
@@ -238,6 +370,79 @@ image_disease_data <- disease_assessment_table %>% # check results and extract m
 #                                                                                h_pal = h, s_pal = s, b_pal = b))
 # toc()
 # 132 sec
+
+# trim images based on drawn bounding box
+
+test <- pliman::image_import("input_images/test_rect.jpg")
+autocrop_test <- pliman::analyze_objects(test, index = "B", ) # used image_binary() to discover "index" argument
+plot_measures(autocrop_test, measure = 'id', col = "black")
+measures <- get_measures(autocrop_test, dpi = 350)
+max(measures$perimeter)
+
+
+
+# blue border image crop dimensions
+
+trim_top = 450
+trim_right = 470
+trim_bottom = 725
+trim_left = 480
+
+# 20220524_Phenotyping image crop dimensions
+
+process_image_pliman(image_file = "../../Image Capture/20220524_Phenotyping/B002_POT1_PL1_00001.JPG",
+                     out_folder = "output/processed_images",
+                     assess_disease = FALSE,
+                     trans = FALSE,
+                     trim_bottom=350, trim_top=273,
+                     trim_left=245, trim_right=300,
+                     h_pal = h, s_pal = s, b_pal = b)
+# find average colour (from NE corner this time)
+avg_bgcolor("./output/processed_images/B002_POT1_PL1_00001_cropped.jpg", "200x200+5300+100" ) #DAD1C8
+
+
+bioassay_test <- list.files("../../Image Capture/20220524_Phenotyping/", ".JPG", full.names = TRUE)[1:20]
+
+# test on one image
+
+process_image_pliman(image_file = bioassay_test[1], 
+                     out_folder = "../../Image Capture/20220524_Phenotyping/processed_images", 
+                     assess_disease = FALSE,
+                     crop = TRUE, 
+                     trim_bottom=350, trim_top=273,
+                     trim_left=245, trim_right=300,
+                     save_cropped = TRUE,
+                     trans = TRUE,
+                     set_fuzz = 35,
+                     reference="#DAD1C8",
+                     start_point = "+5400+3500",
+                     h_pal = h, s_pal = s, b_pal = b)
+
+tic() # start timer
+with_progress({
+  p <- progressor(steps = length(bioassay_test)) # 
+  disease_assessment_table <- bioassay_test %>% 
+    future_map_dfr(.f = ~{
+      p()
+      res_tibble <- process_image_pliman(image_file = .x, 
+                                         out_folder = "../../Image Capture/20220524_Phenotyping/processed_images", 
+                                         assess_disease = TRUE,
+                                         crop = TRUE, 
+                                         trim_bottom=350, trim_top=273,
+                                         trim_left=245, trim_right=300,
+                                         save_cropped = TRUE,
+                                         trans = TRUE,
+                                         set_fuzz = 18,
+                                         reference="#D7CEC5",
+                                         start_point = "+5400+3500",
+                                         h_pal = h, s_pal = s, b_pal = b)
+       # because we put this last in the future_map_dfr, the function returned the progressor step instead of the tibble!
+      return(res_tibble) # this should fix it...
+    }, .options = furrr_options(seed = TRUE)) # this removes the annoying warning about the seed (though I don'tthink we're generating any random numbers)
+})
+toc()
+
+
 
 
 
